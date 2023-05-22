bda697cf2f6cfd4758c80e75c94e5bc719d0dc8a
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-22 15:27:38 +0200

    :seedling: [Footer] Footer component, style and test done

---
+++ src/__test__/Footer.test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+
+import { Footer } from "../components/footer/Footer"
+
+describe("when the footer is diplayed", () => {
+
+    it("renders the footer with effective navigation link", () => {
+        
+        render(
+        <BrowserRouter>
+            <Footer />
+        </BrowserRouter>
+    )
+        const link = screen.getByTestId("footer-link")
+
+        expect(link).toBeInTheDocument()
+        expect(link).toHaveAttribute("href", "/")
+        fireEvent.click(link)
+        expect(window.location.pathname).toBe("/")
+    })
+})
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react"
+import Logo from "../../assets/img/LOGO-BLACK.svg"
+import styles from "./Footer.module.scss"
 
 export const Footer: React.FC<{}> = () => {
     return (
-        <div>
-            FOOTER
-        </div>
+        <footer className={styles.footer}>
+            <a href="/" data-testid="footer-link">
+                <img src={Logo} alt="Logo de Kasa" className={styles.image} />
+            </a>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
     )
 }
