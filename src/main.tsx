d9f22af39635ae9364a86adbc049f7b05798f9e5
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-22 13:21:44 +0200

    :lipstick: [style] style init
    creation of styles path, mixins, variables and reset folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import "./index.css"
+import "./style/style.css"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
