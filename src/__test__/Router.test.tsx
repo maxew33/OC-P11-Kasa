78733dd16950d30072d007269d9777c02aae6415
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-25 15:13:27 +0200

    :white_check_mark: [Router] pass test

--- src/__test__/Router.test.tsx
+++ src/__test__/Router.test.tsx
@@ -1,51 +1,25 @@
-import { describe, expect, it } from "vitest"
-import { render, screen } from "@testing-library/react"
-import { MemoryRouter } from "react-router-dom"
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Router from "../router/Router";
 
-import Router from "../router/Router"
+describe("Router", () => {
+  const testRoutes = [
+    { path: "/", component: "home-page" },
+    { path: "/about", component: "about" },
+    { path: "/lodging/b9123946", component: "lodging" },
+    { path: "/404", component: "error" },
+  ];
 
-describe("when I change the route", () => {
-    it('it should render Home page for "/" route', async () => {
-        render(
-            <MemoryRouter initialEntries={["/"]}>
-                <Router />
-            </MemoryRouter>
-        )
+  testRoutes.forEach(({ path, component }) => {
+    it(`renders ${component} page for "${path}" route`, async () => {
+      render(
+        <MemoryRouter initialEntries={[path]}>
+          <Router />
+        </MemoryRouter>
+      );
 
-        // Vérifier que la page d'accueil est rendue
-        expect(screen.getByTestId("home-page")).toBeInTheDocument()
-    })
-
-    it('it should render About page for "/about" route', async () => {
-        render(
-            <MemoryRouter initialEntries={["/about"]}>
-                <Router />
-            </MemoryRouter>
-        )
-
-        // Vérifier que la page "About" est rendue
-        expect(screen.getByTestId("about")).toBeInTheDocument()
-    })
-
-    it('it should render Lodging page for "/lodging" route', async () => {
-        render(
-            <MemoryRouter initialEntries={["/lodging/0"]}>
-                <Router />
-            </MemoryRouter>
-        )
-
-        // Vérifier que la page "Lodging" est rendue
-        expect(screen.getByTestId("lodging")).toBeInTheDocument()
-    })
-
-    it("it should render Error page for unknown routes", async () => {
-        render(
-            <MemoryRouter initialEntries={["/unknown"]}>
-                <Router />
-            </MemoryRouter>
-        )
-
-        // Vérifier que la page "Error" est rendue pour les routes inconnues
-        expect(screen.getByTestId("error")).toBeInTheDocument()
-    })
-})
+      expect(screen.getByTestId(component)).toBeInTheDocument();
+    });
+  });
+});
