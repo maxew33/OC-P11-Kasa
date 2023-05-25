dc20e271c9c60f8862dd286e8c3996b48d3c9a64
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-25 15:34:48 +0200

    :seedling: [About] page done

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,9 +1,24 @@
-import React from "react"
+import Banner from '../../components/banner/Banner'
+import Dropdown from '../../components/dropdown/Dropdown'
+import aboutData from '../../data/aboutData.json'
 
 export default function About() {
+
     return (
         <>
-            <div data-testid="about">ABOUT</div>
+            <main data-testid="about">
+                <Banner text="" src="/src/assets/img/about-banner-img.png" />
+                {aboutData.map((dropdown, idx) => {
+                    return (
+                        <Dropdown
+                            title={dropdown.title}
+                            size="large"
+                            content={dropdown.content}
+                            key={idx}
+                        />
+                    )
+                })}
+            </main>
         </>
     )
 }
