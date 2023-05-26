724fef0c36d3fb9b3539dc3d61a28b96d7906f34
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-26 09:53:50 +0200

    :lipstick: [Dropdown] style for desktop done
    todo : accordion animation

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,21 +1,38 @@
-import styles from "./dropdown.module.css";
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import styles from './dropdown.module.css'
 
 type DropdownProps = {
-  title: string;
-  size: string;
-  content: string[];
-};
+    title: string
+    size: string
+    content: string[]
+}
 
 export default function Dropdown(props: DropdownProps) {
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <h3>{props.size}</h3>
-      <div>
-        {props.content.map((item) => {
-          return <div className="item">{item}</div>;
-        })}
-      </div>
-    </div>
-  );
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <div
+            className={`${
+                props.size === 'large' ? styles.large : styles.medium
+            } ${styles.dropdown}`}
+        >
+            <div className={styles.title} onClick={handleClick}>
+                <h2>{props.title}</h2>
+                <span className={`${open && styles.rotate} ${styles.arrow}`}>
+                <FontAwesomeIcon icon={faChevronDown} />
+                </span>
+            </div>
+            <div className={`${open && styles.open} ${styles.content}`}>
+                {props.content.map((item) => {
+                    return <div className="item">{item}</div>
+                })}
+            </div>
+        </div>
+    )
 }
