dbb36f6096220d3c83ff32b344fbe07410c59954
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-26 10:25:43 +0200

    :lipstick: [Home] style done for desktop

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,15 @@
-import styles from "./Home.module.css"
-import lodgingData from "../../data/lodgingData.json"
-import Card from "../../components/card/Card"
-import Banner from "../../components/banner/Banner"
+import styles from './Home.module.css'
+import lodgingData from '../../data/lodgingData.json'
+import Card from '../../components/card/Card'
+import Banner from '../../components/banner/Banner'
 
 export default function Home() {
-
     return (
         <main data-testid="home-page">
-            <Banner text = "Chez vous et partout ailleurs" src = "/src/assets/img/home-banner-img.png"/>
+            <Banner
+                text="Chez vous et partout ailleurs"
+                src="/src/assets/img/home-banner-img.png"
+            />
             <section className={styles.section}>
                 {lodgingData.map((data, idx) => {
                     return (
