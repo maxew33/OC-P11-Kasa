4eff5913069adf6bff8f04d460eedb9357a9c6db
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-26 13:07:39 +0200

    :recycle: Refactor and clean code

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,11 +2,13 @@ import Banner from '../../components/banner/Banner'
 import Dropdown from '../../components/dropdown/Dropdown'
 import aboutData from '../../data/aboutData.json'
 
+import styles from "./About.module.css"
+
 export default function About() {
 
     return (
         <>
-            <main data-testid="about">
+            <main data-testid="about" className={styles.container}>
                 <Banner text="" src="/src/assets/img/about-banner-img.png" />
                 {aboutData.map((dropdown, idx) => {
                     return (
--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,9 +1,13 @@
-import React from "react"
+import styles from './Error.module.css'
 
 export default function Error() {
     return (
-        <>
-            <div data-testid="error">404</div>
-        </>
+        <main data-testid="error" className={styles.container}>
+            <p>
+                <h1>404</h1>
+                <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            </p>
+            <a href="/">retour à la page d'accueil</a>
+        </main>
     )
 }
