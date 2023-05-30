b3e78c5362f1a7c7aae035026e31001a06e47a15
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-05-30 10:17:52 +0200

    :iphone: [Error] mobile style done

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -3,11 +3,11 @@ import styles from './Error.module.css'
 export default function Error() {
     return (
         <main data-testid="error" className={styles.container}>
-            <p>
+            <div>
                 <h1>404</h1>
                 <h2>Oups! La page que vous demandez n'existe pas.</h2>
-            </p>
-            <a href="/">retour à la page d'accueil</a>
+            </div>
+            <a href="/">retour sur la page d'accueil</a>
         </main>
     )
 }
