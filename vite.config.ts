fb214d555f3755605598b49bbe3ceb48848b2818
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-06-01 17:56:06 +0200

    :wrench: [vite.config.ts] base added for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/OC-P11-Kasa/',
   plugins: [react()],
   test: {
     environment: "jsdom",
